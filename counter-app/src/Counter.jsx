6959faef19028662560dd1da3ad60f89d0cdcc33
Mohd Shamsher <100463545+ShamsherCoder09@users.noreply.github.com>
2024-02-16 06:58:29 +0000

    again proect done

--- counter-app/src/Counter.jsx
+++ counter-app/src/Counter.jsx
@@ -11,6 +11,28 @@ function Counter(){
     // function decrease(){
     //     setCount(count-1);
     // }
+
+    // make counter the functionality by suing call back function
+    function increase(){
+        // setCount((previuosState)=>{
+        //     return previuosState+1;
+        // })
+
+        setCount(previuosState => previuosState+1);
+    }
+
+    function reset(){
+        setCount(0);
+    }
+
+    function decrease(){
+        // setCount((previuosState)=>{
+        //     return previuosState-1;
+        // })
+
+        setCount (previuosState => previuosState -1);
+    }
+
     return (
         <>
         <h1>Counter App</h1> 
@@ -20,7 +42,7 @@ function Counter(){
         <button onClick={decrease}>Decrease</button> */}
 
         {/* all code write by using arrow function */}
-        <button onClick={()=>{
+        {/* <button onClick={()=>{
             setCount(count+1);
         }}>Increase</button>
         <button onClick={()=>{
@@ -28,7 +50,13 @@ function Counter(){
         }}>Reset</button>
         <button onClick={()=>{
             setCount(count-1);
-        }}>Decrease</button>
+        }}>Decrease</button> */}
+
+        {/* make the program by using callback function */}
+
+        <button onClick={increase}>Increase</button>
+        <button onClick={reset}> Reset</button>
+        <button onClick={decrease}>Decrease</button>
         </>
     )
 }
